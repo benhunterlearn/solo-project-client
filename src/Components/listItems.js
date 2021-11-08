d0888d9785f90a5842d0444fc6fb20cadfc3e5a6
Ben Hunter <learntocode@benhunter.me>
2021-11-08 10:30:35 -0600

    Add templates from MUI Dashboard example. Refactor to mock a basic interface.

---
+++ src/Components/listItems.js
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PeopleIcon from '@mui/icons-material/People';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import LayersIcon from '@mui/icons-material/Layers';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import AddAlertIcon from '@mui/icons-material/AddAlert';
+
+export const mainListItems = (
+    <div>
+        <ListItem button>
+            <ListItemIcon>
+                <DashboardIcon/>
+            </ListItemIcon>
+            <ListItemText primary="Alarms"/>
+        </ListItem>
+        <ListItem button>
+            <ListItemIcon>
+                <AddAlertIcon/>
+            </ListItemIcon>
+            <ListItemText primary="Add a new Alarm"/>
+        </ListItem>
+    </div>
+);
